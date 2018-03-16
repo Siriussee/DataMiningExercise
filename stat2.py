06debd4f760253e1e1f0706b22830027faed619b
SiriusSee <Sirius_See@outlook.com>
2018-03-16 13:45:45 +0800

    add get status

--- stat2.py
+++ stat2.py
@@ -1,6 +1,30 @@
 import pandas as pd
 import numpy as np
 
+def status(x) : 
+    return pd.Series([x.count(),x.min(),x.quantile(.25),x.median(),
+                      x.quantile(.75),x.mean(),x.max(),x.mad(),x.var(),
+                      x.std()],index=['总数','最小值','25%分位数',
+                    '中位数','75%分位数','均值','最大值','平均绝对偏差','方差','标准差'])
+
+def get_status_according_to_road_id(road_id_x):
+    travel_time_when_road_id_is_x = []
+    for each in single_travel_seq:
+        id = each.split('#')[0]
+        if id == road_id_x:
+            travel_time_when_road_id_is_x.append(each.split('#')[2])
+
+    travel_time_when_road_id_is_x = map(eval,travel_time_when_road_id_is_x)
+    print 'test:'
+    # print travel_time_when_road_id_is_x
+    series_travel_time_when_road_id_is_x = pd.Series(travel_time_when_road_id_is_x)
+    print 'statistic of road',
+    print road_id_x
+    print status(series_travel_time_when_road_id_is_x)
+
+def get_status_according_to_date(date_x):
+    
+
 # read file
 df1 = pd.read_csv("data/test_case.csv")
 
@@ -12,12 +36,24 @@ df1 = pd.read_csv("data/test_case.csv")
 # 122#2016-07-19 00:15:02#32.85
 
 travel_seq = df1["travel_seq"]
-print travel_seq.head(1)
+# print travel_seq.head(1)
 
 # test str.split('mark')
 # print travel_seq[0].split(';')[0].split('#')[0]
 
-road_id_arr = np.array([])
-np.insert(road_id_arr,0,travel_seq[0].split(';')[0].split('#')[0])
+single_travel_seq = []
+road_id = []
 
-print road_id_arr
+for each_travel_seq in travel_seq:
+    temp = each_travel_seq.split(';')
+    for each in temp:
+        single_travel_seq.append(each)
+        road_id.append(each.split('#')[0])
+
+unique_road_id = np.unique(road_id)
+# print unique_road_id
+print 'unique road id count = ',
+print len(unique_road_id)
+
+for each in unique_road_id:
+    get_status_according_to_road_id(each)
